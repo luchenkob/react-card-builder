28e5e41a431489cc4ffa62a12cccee886887c684
Bogdan Luchenko <luchenkob@gmail.com>
2020-03-30 19:13:41 +0300

    Added GTM

--- src/cta-builder.js
+++ src/cta-builder.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import CtaBuilder from "./containers/CtaBuilder.jsx";
 import "./styles/styles.scss";
 import '@simonwep/pickr/dist/themes/monolith.min.css';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {gtmId: 'GTM-NDQ8P2'}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(<CtaBuilder />, document.getElementById("root"));
 
